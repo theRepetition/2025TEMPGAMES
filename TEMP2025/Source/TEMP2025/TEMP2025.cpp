5c6de838e6fbe1a4fc4713fb66cebaeab3868e26
ahn <dudska0202@naver.com>
2025-01-06 02:56:46 +0900

    강공 구현을 위해 mixamo에서 애니메이션 리타게팅
    강공 구현 준비 끝

---
+++ TEMP2025/Source/TEMP2025/TEMP2025.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TEMP2025.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TEMP2025, "TEMP2025" );
