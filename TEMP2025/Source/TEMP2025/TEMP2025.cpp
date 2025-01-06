cb2e039188196701f1d2ae266077986441b4dde1
ahn <dudska0202@naver.com>
2025-01-06 13:14:06 +0900

    Revert "강공 구현을 위해 mixamo에서 애니메이션 리타게팅"
    This reverts commit 5c6de838e6fbe1a4fc4713fb66cebaeab3868e26.

--- TEMP2025/Source/TEMP2025/TEMP2025.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "TEMP2025.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TEMP2025, "TEMP2025" );
