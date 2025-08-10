161843e65a0a1c198929b27e5fefb4a7c2610f8b
ahn <dudska0202@naver.com>
2025-08-10 20:57:26 +0900

    Revert "5.6으로 버전 올림"
    This reverts commit 0662f4826c1cd8e24d73fccfb00d002b877bfa96.

--- TEMP2025/Source/TEMP2025Editor.Target.cs
+++ TEMP2025/Source/TEMP2025Editor.Target.cs
@@ -8,10 +8,8 @@ public class TEMP2025EditorTarget : TargetRules
 	public TEMP2025EditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
-        IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_6;
-        CppStandard = CppStandardVersion.Cpp20;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
 
-        ExtraModuleNames.AddRange( new string[] { "TEMP2025" } );
+		ExtraModuleNames.AddRange( new string[] { "TEMP2025" } );
 	}
 }
