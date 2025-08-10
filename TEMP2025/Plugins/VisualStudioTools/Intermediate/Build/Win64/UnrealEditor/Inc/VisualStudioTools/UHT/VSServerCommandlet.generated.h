97df3e3461b2dd087dea0459dbb81478e51fb06b
ahn <dudska0202@naver.com>
2025-08-10 21:09:40 +0900

    Revert "ㅅ드ㅔ2"
    This reverts commit df1c322695324500d0f460deedf2f999f3a542ec.

---
+++ TEMP2025/Plugins/VisualStudioTools/Intermediate/Build/Win64/UnrealEditor/Inc/VisualStudioTools/UHT/VSServerCommandlet.generated.h
@@ -0,0 +1,60 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "VSServerCommandlet.h"
+
+#ifdef VISUALSTUDIOTOOLS_VSServerCommandlet_generated_h
+#error "VSServerCommandlet.generated.h already included, missing '#pragma once' in VSServerCommandlet.h"
+#endif
+#define VISUALSTUDIOTOOLS_VSServerCommandlet_generated_h
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+
+// ********** Begin Class UVSServerCommandlet ******************************************************
+VISUALSTUDIOTOOLS_API UClass* Z_Construct_UClass_UVSServerCommandlet_NoRegister();
+
+#define FID_TempGames_2025TEMPGAMES_TEMP2025_Plugins_VisualStudioTools_Source_VisualStudioTools_Private_VSServerCommandlet_h_19_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUVSServerCommandlet(); \
+	friend struct Z_Construct_UClass_UVSServerCommandlet_Statics; \
+	static UClass* GetPrivateStaticClass(); \
+	friend VISUALSTUDIOTOOLS_API UClass* Z_Construct_UClass_UVSServerCommandlet_NoRegister(); \
+public: \
+	DECLARE_CLASS2(UVSServerCommandlet, UCommandlet, COMPILED_IN_FLAGS(0 | CLASS_Transient), CASTCLASS_None, TEXT("/Script/VisualStudioTools"), Z_Construct_UClass_UVSServerCommandlet_NoRegister) \
+	DECLARE_SERIALIZER(UVSServerCommandlet)
+
+
+#define FID_TempGames_2025TEMPGAMES_TEMP2025_Plugins_VisualStudioTools_Source_VisualStudioTools_Private_VSServerCommandlet_h_19_ENHANCED_CONSTRUCTORS \
+	/** Deleted move- and copy-constructors, should never be used */ \
+	UVSServerCommandlet(UVSServerCommandlet&&) = delete; \
+	UVSServerCommandlet(const UVSServerCommandlet&) = delete; \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UVSServerCommandlet); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UVSServerCommandlet); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(UVSServerCommandlet) \
+	NO_API virtual ~UVSServerCommandlet();
+
+
+#define FID_TempGames_2025TEMPGAMES_TEMP2025_Plugins_VisualStudioTools_Source_VisualStudioTools_Private_VSServerCommandlet_h_15_PROLOG
+#define FID_TempGames_2025TEMPGAMES_TEMP2025_Plugins_VisualStudioTools_Source_VisualStudioTools_Private_VSServerCommandlet_h_19_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_TempGames_2025TEMPGAMES_TEMP2025_Plugins_VisualStudioTools_Source_VisualStudioTools_Private_VSServerCommandlet_h_19_INCLASS_NO_PURE_DECLS \
+	FID_TempGames_2025TEMPGAMES_TEMP2025_Plugins_VisualStudioTools_Source_VisualStudioTools_Private_VSServerCommandlet_h_19_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+class UVSServerCommandlet;
+
+// ********** End Class UVSServerCommandlet ********************************************************
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_TempGames_2025TEMPGAMES_TEMP2025_Plugins_VisualStudioTools_Source_VisualStudioTools_Private_VSServerCommandlet_h
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ TEMP2025/Plugins/VisualStudioTools/Intermediate/Build/Win64/UnrealEditor/Inc/VisualStudioTools/UHT/VisualStudioToolsCommandletBase.generated.h
@@ -0,0 +1,60 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "VisualStudioToolsCommandletBase.h"
+
+#ifdef VISUALSTUDIOTOOLS_VisualStudioToolsCommandletBase_generated_h
+#error "VisualStudioToolsCommandletBase.generated.h already included, missing '#pragma once' in VisualStudioToolsCommandletBase.h"
+#endif
+#define VISUALSTUDIOTOOLS_VisualStudioToolsCommandletBase_generated_h
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+
+// ********** Begin Class UVisualStudioToolsCommandletBase *****************************************
+VISUALSTUDIOTOOLS_API UClass* Z_Construct_UClass_UVisualStudioToolsCommandletBase_NoRegister();
+
+#define FID_TempGames_2025TEMPGAMES_TEMP2025_Plugins_VisualStudioTools_Source_VisualStudioTools_Private_VisualStudioToolsCommandletBase_h_14_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUVisualStudioToolsCommandletBase(); \
+	friend struct Z_Construct_UClass_UVisualStudioToolsCommandletBase_Statics; \
+	static UClass* GetPrivateStaticClass(); \
+	friend VISUALSTUDIOTOOLS_API UClass* Z_Construct_UClass_UVisualStudioToolsCommandletBase_NoRegister(); \
+public: \
+	DECLARE_CLASS2(UVisualStudioToolsCommandletBase, UCommandlet, COMPILED_IN_FLAGS(CLASS_Abstract | CLASS_Transient), CASTCLASS_None, TEXT("/Script/VisualStudioTools"), Z_Construct_UClass_UVisualStudioToolsCommandletBase_NoRegister) \
+	DECLARE_SERIALIZER(UVisualStudioToolsCommandletBase)
+
+
+#define FID_TempGames_2025TEMPGAMES_TEMP2025_Plugins_VisualStudioTools_Source_VisualStudioTools_Private_VisualStudioToolsCommandletBase_h_14_ENHANCED_CONSTRUCTORS \
+	/** Deleted move- and copy-constructors, should never be used */ \
+	UVisualStudioToolsCommandletBase(UVisualStudioToolsCommandletBase&&) = delete; \
+	UVisualStudioToolsCommandletBase(const UVisualStudioToolsCommandletBase&) = delete; \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UVisualStudioToolsCommandletBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UVisualStudioToolsCommandletBase); \
+	DEFINE_ABSTRACT_DEFAULT_CONSTRUCTOR_CALL(UVisualStudioToolsCommandletBase) \
+	NO_API virtual ~UVisualStudioToolsCommandletBase();
+
+
+#define FID_TempGames_2025TEMPGAMES_TEMP2025_Plugins_VisualStudioTools_Source_VisualStudioTools_Private_VisualStudioToolsCommandletBase_h_10_PROLOG
+#define FID_TempGames_2025TEMPGAMES_TEMP2025_Plugins_VisualStudioTools_Source_VisualStudioTools_Private_VisualStudioToolsCommandletBase_h_14_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_TempGames_2025TEMPGAMES_TEMP2025_Plugins_VisualStudioTools_Source_VisualStudioTools_Private_VisualStudioToolsCommandletBase_h_14_INCLASS_NO_PURE_DECLS \
+	FID_TempGames_2025TEMPGAMES_TEMP2025_Plugins_VisualStudioTools_Source_VisualStudioTools_Private_VisualStudioToolsCommandletBase_h_14_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+class UVisualStudioToolsCommandletBase;
+
+// ********** End Class UVisualStudioToolsCommandletBase *******************************************
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_TempGames_2025TEMPGAMES_TEMP2025_Plugins_VisualStudioTools_Source_VisualStudioTools_Private_VisualStudioToolsCommandletBase_h
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
